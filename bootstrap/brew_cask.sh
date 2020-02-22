109501b3239575ea06bb781949e3961035c5de5e
John Hwang <johnyhwang@gmail.com>
2020-02-22 16:51:42 -0500

    Update default apps for MacBook Pro 16" 2019

--- bootstrap/brew_cask.sh
+++ bootstrap/brew_cask.sh
@@ -7,12 +7,11 @@
 
 # Install native apps
 
-brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
 
 # daily
-brew cask install spectacle
-brew cask install magicprefs
+brew cask install rectangle
+brew cask install multitouch
 
 brew cask install lastpass
 brew cask install rescuetime
@@ -31,6 +30,7 @@ brew cask install mindnode-pro
 brew tap colindean/fonts-nonfree
 brew cask install font-microsoft-office
 
+brew tap homebrew/cask-fonts
 brew cask install \
   font-anonymous-pro \
   font-dejavu-sans-mono-for-powerline \
@@ -53,16 +53,17 @@ brew cask install \
 brew cask install iterm2
 brew cask install sublime-text
 brew cask install atom
+brew cask install visual-studio-code
 brew cask install anvil           # POW management
 
 brew cask install sequel-pro
 brew cask install psequel
 
-brew cask install github-desktop
+brew cask install github
 
 brew cask install virtualbox
 brew cask install virtualbox-extension-pack
-brew cask install docker-toolbox
+brew cask install docker
 
 # image
 #brew cask install imagealpha
@@ -81,16 +82,14 @@ brew cask install docker-toolbox
 #brew cask install miro-video-converter
 # brew cask install horndis               # android usb tethering
 
-#brew cask install flickr-uploadr
 #brew cask install fluid
 
 # browsers
-brew cask install chrome
-#brew cask install firefox
+brew cask install google-chrome
+brew cask install firefox
 #brew cask install firefoxdeveloperedition
 
 brew cask install skype
-#brew cask install screenhero
 
 #brew cask install textexpander3
 
@@ -113,30 +112,49 @@ brew cask install chromecast
 brew cask install keycastr
 
 #brew cask install selfcontrol
-#brew cask install hazel
-#brew cask install hocus-focus
-#brew cask install caffine
+brew cask install hazel
+brew cask install hocus-focus
+brew cask install caffine
 # brew cask install pophub
 
 #brew cask install carbon-copy-cloner
 
-brew cask install scansnap-manager-s300m-s510m
 brew cask install microsoft-office
-# brew cask install adobe-creative-cloud
+brew cask install adobe-creative-cloud
 # brew cask install adobe-photoshop-cs5
 # brew cask install adobe-design-standard-cs6
 
-# brew cask install wacom-tablet
+brew cask install wacom-tablet
 # brew cask install xscope
 
 #screensavers
 brew cask install aerial
 brew cask install fliqio
 
-brew install figma
-brew install appcleaner
-brew install backdrop
-brew install coffitivity
+brew cask install figma
+brew cask install appcleaner
+brew cask install backdrop
+brew cask install coffitivity
 
+brew cask install backblaze
+brew cask install audio-hijack
+brew cask install camtwist
 
+brew cask install drawio
+brew cask install flume
+brew cask install obs
+brew cask install local
+brew cask install station
+brew cask install steam
+
+brew cask install levelator
+brew cask install zoom
+
+brew tap homebrew/cask-drivers
+brew cask install fujitsu-scansnap-manager-s1300
+brew cask install caldigit-thunderbolt-charging
+brew cask install canon-eos-utility
+brew cask install elgato-stream-deck
+brew cask install insta360-studio
+brew cask install wacom-tablet
 
